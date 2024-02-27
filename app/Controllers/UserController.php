64858e223aa876caa6a12d7a34ffd703cfe6b148
HIBISCUS0915 <shiellamaevillamel20@gmail.com>
2024-02-27 09:23:04 +0800

    Villamel_ci working

---
+++ app/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class UserController extends BaseController
+{
+    public function index(): string
+    {
+        return view('welcome_message');
+    }
+    public function addUser(){
+        return view('users/add');
+    }
+}
